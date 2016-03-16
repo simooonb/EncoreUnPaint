1a1f55c14226ed4cb2d2378456b303cb3bc4050e
Aubin.Vandenbilcke <aubin.vandenbilcke@isen-lille.fr>
2016-03-16 11:07:23 +0100

    UPdating current status

--- src/client/controller/ToolbarController.java
+++ src/client/controller/ToolbarController.java
@@ -1,5 +1,6 @@
 package client.controller;
 
+import client.view.DrawingContainerView;
 import client.view.StatusAreaView;
 import client.view.ToolbarView;
 
@@ -9,41 +10,47 @@ import java.awt.*;
 public class ToolbarController {
     private ToolbarView toolbarView;
     private StatusAreaView statusAreaView;
+    private DrawingContainerView drawingContainerView;
     private Color selectedColor = Color.BLACK;
 
-    public ToolbarController(ToolbarView toolbarView, StatusAreaView statusAreaView) {
+    public ToolbarController(ToolbarView toolbarView, StatusAreaView statusAreaView, DrawingContainerView drawingContainerView) {
 
         this.toolbarView = toolbarView;
         this.statusAreaView = statusAreaView;
+        this.drawingContainerView = drawingContainerView;
         this.toolbarView.getSelectForm().addActionListener(e -> {
-            System.out.println("select form");
+            drawingContainerView.setCurrentStatus("selection");
             statusAreaView.editStatus("Selection form");
         });
 
         this.toolbarView.getFillForm().addActionListener(e -> {
-            System.out.println("fill form");
+            drawingContainerView.setCurrentStatus("fill");
             statusAreaView.editStatus("Fill form");
         });
 
         this.toolbarView.getDrawOval().addActionListener(e -> {
-            System.out.println("draw oval");
+            drawingContainerView.setCurrentStatus("oval");
             statusAreaView.editStatus("Oval");
         });
 
         this.toolbarView.getDrawLine().addActionListener(e -> {
-            System.out.println("draw line");
+            drawingContainerView.setCurrentStatus("line");
             statusAreaView.editStatus("Line");
         });
 
         this.toolbarView.getDrawRectangle().addActionListener(e -> {
-            System.out.println("draw rectangle");
+            drawingContainerView.setCurrentStatus("rectangle");
             statusAreaView.editStatus("Rectangle");
         });
 
         this.toolbarView.getColorChooser().addActionListener(e -> {
-            System.out.println("color chooser");
+            drawingContainerView.setCurrentStatus("color");
             selectedColor = JColorChooser.showDialog(null, "Choose a color", selectedColor);
             statusAreaView.editStatus("Color");
         });
+    }
+
+    public Color getSelectedColor() {
+        return selectedColor;
     }
 }
--- src/client/view/DrawingContainerView.java
+++ src/client/view/DrawingContainerView.java
@@ -4,8 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DrawingContainerView extends JPanel {
+    private String currentStatus = "None";
     public DrawingContainerView() {
         setBackground(Color.white);
         setSize(500, 500);
     }
+
+    public String getCurrentStatus() { return currentStatus; }
+
+    public void setCurrentStatus(String currentStatus) { this.currentStatus = currentStatus; }
 }
