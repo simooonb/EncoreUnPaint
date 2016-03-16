08e58e745a2314fbc5ae1eeff2aab98864ca45b0
Aubin.Vandenbilcke <aubin.vandenbilcke@isen-lille.fr>
2016-03-16 10:00:05 +0100

    Toolbar view

--- src/client/view/ToolbarView.java
+++ src/client/view/ToolbarView.java
@@ -1,6 +1,7 @@
 package client.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ToolbarView extends JToolBar{
     private JButton selectForm;
@@ -8,15 +9,25 @@ public class ToolbarView extends JToolBar{
     private JButton drawOval;
     private JButton drawRectangle;
     private JButton drawLine;
+    private JButton colorChooser;
 
     public ToolbarView(){
         super(null, JToolBar.VERTICAL);
+        this.setFloatable(true);
+        this.setRollover(true);
+        this.setBorderPainted(true);
         initButton();
         this.add(selectForm);
+        this.addSeparator();
         this.add(fillForm);
+        this.addSeparator();
         this.add(drawOval);
+        this.addSeparator();
         this.add(drawRectangle);
+        this.addSeparator();
         this.add(drawLine);
+        this.addSeparator();
+        this.add(colorChooser);
     }
 
     private void initButton(){
@@ -25,5 +36,6 @@ public class ToolbarView extends JToolBar{
         drawOval = new JButton("Oval");
         drawRectangle = new JButton("Rectangle");
         drawLine = new JButton("Line");
+        colorChooser = new JButton("Color");
     }
 }
