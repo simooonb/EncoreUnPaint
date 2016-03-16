c29bbcd475a771def14888dc8e06dcde0f900b3c
Aubin.Vandenbilcke <aubin.vandenbilcke@isen-lille.fr>
2016-03-16 09:41:09 +0100

    Start Toolbar

--- src/client/view/ToolbarView.java
+++ src/client/view/ToolbarView.java
@@ -3,8 +3,27 @@ package client.view;
 import javax.swing.*;
 
 public class ToolbarView extends JToolBar{
+    private JButton selectForm;
+    private JButton fillForm;
+    private JButton drawOval;
+    private JButton drawRectangle;
+    private JButton drawLine;
 
     public ToolbarView(){
-        
+        super(null, JToolBar.VERTICAL);
+        initButton();
+        this.add(selectForm);
+        this.add(fillForm);
+        this.add(drawOval);
+        this.add(drawRectangle);
+        this.add(drawLine);
+    }
+
+    private void initButton(){
+        selectForm = new JButton("Select");
+        fillForm = new JButton("Fill");
+        drawOval = new JButton("Oval");
+        drawRectangle = new JButton("Rectangle");
+        drawLine = new JButton("Line");
     }
 }
