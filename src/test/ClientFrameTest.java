8ae69c69ea0a2c0fab94c1e5dfb6a25e9c3e427b
snatz <simon.bar@isen-lille.fr>
2016-03-16 09:42:16 +0100

    Test

---
+++ src/test/ClientFrameTest.java
@@ -0,0 +1,9 @@
+package test;
+
+import client.view.ClientFrameView;
+
+public class ClientFrameTest {
+    public static void main(String[] args) {
+        ClientFrameView frame = new ClientFrameView();
+    }
+}
