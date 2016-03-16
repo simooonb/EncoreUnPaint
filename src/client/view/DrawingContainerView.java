19905028d0eed8c3e94661d713a61edbc0d7b0b2
Aubin.Vandenbilcke <aubin.vandenbilcke@isen-lille.fr>
2016-03-16 11:13:05 +0100

    Updating current color

--- src/client/view/DrawingContainerView.java
+++ src/client/view/DrawingContainerView.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class DrawingContainerView extends JPanel {
     private String currentStatus = "None";
+    private Color currentColor = Color.BLACK;
+
     public DrawingContainerView() {
         setBackground(Color.white);
         setSize(500, 500);
@@ -13,4 +15,8 @@ public class DrawingContainerView extends JPanel {
     public String getCurrentStatus() { return currentStatus; }
 
     public void setCurrentStatus(String currentStatus) { this.currentStatus = currentStatus; }
+
+    public Color getCurrentColor() { return currentColor; }
+
+    public void setCurrentColor(Color currentColor) { this.currentColor = currentColor; }
 }
